cb620e78ab4c7e72ac135856e2dfee0fd73a89a7
Lakshmi <lakshmi@Lakshmis-MacBook-Air.local>
2024-09-02 20:16:27 +0530

    static folder path update

--- src/server.js
+++ src/server.js
@@ -83,17 +83,7 @@ app.use(
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
   })
 );
-/*
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-  next();
-});
 
-*/
 
 // Add headers before the routes are defined
 // jwt authentication
@@ -109,7 +99,7 @@ if (config.env === 'production') {
 app.use('/v1', routes);
 
 // Serve React page
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 app.get('/', cors(), function (req, res) {
   res.header('Access-Control-Allow-Origin', '*');
